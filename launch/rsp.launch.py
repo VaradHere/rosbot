572be9c2ef1332c83fcc66c8159116729f234ae7
VaradHere <varad.sandeep.desai@gmail.com>
2025-04-15 16:11:14 +0530

    humble branch changes

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -21,12 +21,13 @@ def generate_launch_description():
     pkg_path = os.path.join(get_package_share_directory('rosbot'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     # robot_description_config = xacro.process_file(xacro_file, mappings={'use_ros2_control': 'true'})
-    robot_description_config = xacro.process_file(xacro_file).toxml()
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
-    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control])
+    # robot_description_config = xacro.process_file(xacro_file).toxml()
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    # params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
