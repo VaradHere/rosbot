f7f4742e7bdf8a79efa6eb66130f0c8f9319050f
VaradHere <varad.sandeep.desai@gmail.com>
2025-04-18 13:26:46 +0530

    Diffdrive controller modified

--- launch/robot_launch.launch.py
+++ launch/robot_launch.launch.py
@@ -33,7 +33,7 @@ def generate_launch_description():
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     controllers_params_file = os.path.join(
-                  get_package_share_directory(package_name),'config','my_controller.yaml')
+                  get_package_share_directory(package_name),'config','my_controllers.yaml')
 
 
     controller_manager = Node(
@@ -48,7 +48,7 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
     delayed_diff_drive_spawner = RegisterEventHandler(
@@ -59,7 +59,7 @@ def generate_launch_description():
     )
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )   
     delayed_joint_broad_spawner = RegisterEventHandler(
